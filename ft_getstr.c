3dbace6cd8455edce103c5ef32f5f847e1a297b9
Alex Movchan <amovchan@e1r9p12.unit.ua>
2017-03-20 21:47:08 +0200

    test

--- ft_getstr.c
+++ ft_getstr.c
@@ -20,14 +20,14 @@ char *ft_getdouble(va_list *ap, t_srt *lst)
 		return (ft_cast_e(ap, lst));
 	else if (lst->letar == 'E')
 		return (ft_cast_el(ap, lst));
-	/*else if (lst->letar == 'g')
+	else if (lst->letar == 'g')
 		return (ft_str_g(ap, lst));
 	else if (lst->letar == 'G')
-		return (ft_cast_gi(ap, lst));*/
-	else if (lst->letar == 'A')
+		return (ft_cast_gi(ap, lst));
+	/*else if (lst->letar == 'A')
 		return (ft_cast_al(ap, lst));
 	else if (lst->letar == 'a')
-		return (ft_cast_a(ap, lst));
+		return (ft_cast_a(ap, lst));*/
 	else
 		return (NULL);
 }
