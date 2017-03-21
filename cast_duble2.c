2f9d88be350a886bf8d6af41c00b1e5cc14bce9a
Alex Movchan <amovchan@e1r8p9.unit.ua>
2017-03-21 18:13:43 +0200

    test

--- cast_duble2.c
+++ cast_duble2.c
@@ -55,11 +55,11 @@ char	*ft_cast_al(va_list *ap, t_srt *lst)
 
 	i = 0;
 	str = ft_cast_a(ap, lst);
-	/*while (str[i])
+	while (str[i])
 	{
-		if (ft_isalpha(str[i]) == 1)
-			str[i] = (char)ft_toupper(str[i]);
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
 		i++;
-	}*/
+	}
 	return (str);
 }
