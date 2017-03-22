e54bde5cd8f8b530077dc7c6f6a94af7da3d5385
Alex Movchan <amovchan@e1r8p15.unit.ua>
2017-03-22 21:06:16 +0200

    test

--- ft_cast_digit.c
+++ ft_cast_digit.c
@@ -40,7 +40,7 @@ char *ft_cast_di(va_list ap, t_srt *lst)
 	{
 		str = ft_plus2(str, lst);
 
-		if ((lst->accur > lst->width + 1 && lst->space == ' ' && lst->plus == '0')
+		if ((lst->accur > lst->width && lst->space == ' ' && lst->plus == '0')
 				|| (lst->width == -1 && lst->space == ' '))
 			str = ft_space(str, ' ', ((int) ft_strlen(str) + 1), lst);
 		 else if (lst->plus == '0' && lst->accur == -1 && lst->space == ' ')
