4a9da6abe73f8457d5af853aaad585b6fd216053
Alex Movchan <amovchan@e3r5p4.unit.ua>
2017-09-06 19:17:59 +0300

    my_printf

--- ft_prinft/ft_color.c
+++ ft_prinft/ft_color.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "libft.h"
 
 void	ft_color(const char *s, int *i, va_list ap, t_srt *lst)
 {
